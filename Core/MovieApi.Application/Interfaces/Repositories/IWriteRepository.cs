0f4c98525ae98c023c01aca05aa19ed53acc6266
Kaan Kaya <kaankaya0743@gmail.com>
2025-05-13 12:39:35 +0300

    Read ve Write repositoryler yazıldı

---
+++ Core/MovieApi.Application/Interfaces/Repositories/IWriteRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApi.Application.Interfaces.Repositories
+{
+    public interface IWriteRepository<T> where T : class, new()
+    {
+        Task AddAsync(T entity);
+
+        Task AddRangeAsync(IList<T> entities);
+
+        Task<T> UpdateAsync(T entity);
+
+        Task HardDeleteAsync(T entity);
+
+        Task HardDeleteRangeAsync(IList<T> entity);
+    }
+}
