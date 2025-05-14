9ec9f2851fcfc2ea2a26696cd98928e8a6075fa8
Kaan Kaya <kaankaya0743@gmail.com>
2025-05-14 14:24:47 +0300

    CQRS design patternın yapısını kurdum

---
+++ Presentation/MovieApi.Api/Controllers/MoviesController.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MovieApi.Application.Features.Queries.MovieQueries;
+
+namespace MovieApi.Api.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        private readonly IMediator mediator;
+
+        public MoviesController(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllMovie()
+        {
+            var values = await mediator.Send(new GetAllMoviesQuery());
+            return Ok(values);
+        }
+    }
+}
