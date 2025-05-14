3193b0405a06bd8b69a3b176ff2d04884c392532
Kaan Kaya <kaankaya0743@gmail.com>
2025-05-14 10:16:43 +0300

    Unitofwork design pattern yazıldı

---
+++ Infrastructure/MovieApi.Persistence/UnitOfWorks/UnitOfWork.cs
@@ -0,0 +1,47 @@
+﻿using MovieApi.Application.Interfaces.Repositories;
+using MovieApi.Application.Interfaces.UnitOfWorks;
+using MovieApi.Persistence.Context;
+using MovieApi.Persistence.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApi.Persistence.UnitOfWorks
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly AppDbContext appDbContext;
+
+        public UnitOfWork(AppDbContext appDbContext)
+        {
+            this.appDbContext = appDbContext;
+        }
+
+        public ValueTask DisposeAsync()
+        {
+            return appDbContext.DisposeAsync();
+        }
+
+        public IReadRepository<T> GetReadRepository<T>() where T : class, new()
+        {
+            return new ReadRepository<T>(appDbContext);
+        }
+
+        public IWriteRepository<T> GetWriteRepository<T>() where T : class, new()
+        {
+            return new WriteRepository<T>(appDbContext);
+        }
+
+        public int Save()
+        {
+            return appDbContext.SaveChanges();
+        }
+
+        public async Task<int> SaveAsync()
+        {
+            return await appDbContext.SaveChangesAsync();
+        }
+    }
+}
