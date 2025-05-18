91a36932c6e7bf411aceaba84736f8ed1b4c02c1
Kaan Kaya <kaankaya0743@gmail.com>
2025-05-18 16:10:01 +0300

    Register işlemleri yapıldı

---
+++ Core/MovieApi.Application/Features/Handlers/RegisterHandlers/RegisterCommandHandler.cs
@@ -0,0 +1,62 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using MovieApi.Application.Bases;
+using MovieApi.Application.Features.Commands.RegisterCommands;
+using MovieApi.Application.Features.Rules;
+using MovieApi.Application.Interfaces.UnitOfWorks;
+using MovieApi.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApi.Application.Features.Handlers.RegisterHandlers
+{
+    public class RegisterCommandHandler : BaseHandler, IRequestHandler<RegisterCommand, Unit>
+    {
+        private readonly AuthRules authRules;
+        private readonly UserManager<User> userManager;
+        private readonly RoleManager<Role> roleManager;
+
+        public RegisterCommandHandler(AuthRules authRules, UserManager<User> userManager,
+            RoleManager<Role> roleManager , IMapper mapper, IUnitOfWork unitOfWork,
+            IHttpContextAccessor httpContextAccessor) : base(mapper, unitOfWork, httpContextAccessor)
+        {
+            this.authRules = authRules;
+            this.userManager = userManager;
+            this.roleManager = roleManager;
+        }
+        public async Task<Unit> Handle(RegisterCommand request, CancellationToken cancellationToken)
+        {
+            User? usertest = await userManager.FindByEmailAsync(request.Email);
+            await authRules.UserShouldNotBeExist(usertest);
+
+            User user = mapper.Map<User>(request);
+
+            user.UserName = request.Email;
+            user.SecurityStamp = Guid.NewGuid().ToString();  // milisaniyelik işlemler için
+
+
+            IdentityResult result = await userManager.CreateAsync(user , request.Password);
+            if (result.Succeeded) 
+            {
+                if (!await roleManager.RoleExistsAsync("user"))
+                    await roleManager.CreateAsync(new Role
+                    {
+                        Id = Guid.NewGuid(),
+                        Name = "user",
+                        NormalizedName = "USER",
+                        ConcurrencyStamp = Guid.NewGuid().ToString()
+                    });
+
+                await userManager.AddToRoleAsync(user, "user");
+            }
+
+            return Unit.Value;
+
+        }
+    }
+}
---
+++ Presentation/MovieApi.Api/Controllers/AuthController.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MovieApi.Application.Features.Commands.RegisterCommands;
+
+namespace MovieApi.Api.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IMediator mediator;
+
+        public AuthController(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterCommand command)
+        {
+            await mediator.Send(command);
+            return StatusCode(StatusCodes.Status201Created);
+        }
+    }
+}
